512f11b5610a8e6a33818114d5dbb75d54b23455
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-01-09 11:35:16 +0530

    Modify Add patch API

--- src/app.js
+++ src/app.js
@@ -1,30 +1,82 @@
-const express = require('express');
-const connectdb= require('./config/database');
+const express = require("express");
+const connectdb = require("./config/database");
 const app = express();
-const User = require('./models/user'); 
+const User = require("./models/user");
 
 app.use(express.json());
 
-connectdb().then(() => {
-    console.log('Database connected');
+connectdb()
+  .then(() => {
+    console.log("Database connected");
     app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-app.post('/signup', (req, res) => {    
-    const user = new User(req.body);
-    user.save().then((user) => {
-        res.status(201).send("User created");
-    }).catch((err) => {
-        res.status(500).send(err);
+      console.log("Server is running on port 3000");
     });
 
-});
+    app.post("/signup", (req, res) => {
+      const user = new User(req.body);
+      user
+        .save()
+        .then((user) => {
+          res.status(201).send("User created");
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    });
+    //find user with email
+    app.get("/findUser", async (req, res) => {
+      const userEmail = req.body.email;
+      const Users = await User.find({ emailId: userEmail });
+      if (Users.length < 0) {
+        res.status(404).send("User not found");
+      } else {
+        res.status(200).send(Users);
+      }
+    });
+    // find user wose age is greater than number
 
+    app.get("/findByAge", async (req, res) => {
+      const age = req.body.age;
+      const Users = await User.find({ age: { $gt: age } });
+      console.log(Users);
+      if (Users.length < 0) {
+        res.status(404).send("User not found");
+      } else {
+        res.status(200).send(Users);
+      }
+    });
 
-}).catch((err) => {
-    console.log("Database not connected",err);
-});
+    //update user
+    app.patch("/updateUser", async (req, res) => {
+      const userId = req.body.userId;
+      const data = req.body;
 
-
-
+      const ALLOWED_UPDATES = [
+        "userId",
+        "age",
+        "gender",
+        "photoUrl",
+        "about",
+        "skills",
+      ];
+      const isUpdateAllowed = Object.keys(data).every((update) =>
+        ALLOWED_UPDATES.includes(update)
+      );
+      if (!isUpdateAllowed) {
+        return res.status(400).send({ error: "Invalid update", error });
+      }
+      try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+          returnDocument: "after",
+          runValidators: true,
+        });
+        console.log(user)
+        res.send("User Updated Successfully");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log("Database not connected", err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-   firstName:{
-    type: String,
-    required: true
-   },
-   lastName:{
-    type: String,
-    required: true
-   },
-   emailId: {
+const userSchema = new mongoose.Schema({
+  firstName: {
     type: String,
     required: true,
-    unique: true
-   },
-   password: {
+    minLength: 2,
+    maxLength: 50,
+  },
+  lastName: {
     type: String,
-    required: true
-   }
-,
-age: {
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
     type: Number,
-    required: true
-}
-});
+    min:18,
+    max:100,
+  },
+  gender:{
+    type: String,
+    validate(value){
+      if(["male", "female", "other"].includes(value)){
+        throw new Error("Gender should be male, female or other");
+      }
+    }
+  },
+  photoUrl:{
+    type: String
+  },
+  about:{
+    type: String,
+    default: "I am a developer"
+  },
+  skills:{
+    type: [String],
+    uniqueItems: true
+  },
+},
+{
+    timestamps: true
+  },
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
